8f6e9c13b6b1c7ddfbd7ae8411390a09eb50dce4
Etherrreal <65111206+lifeafter619@users.noreply.github.com>
2023-04-06 17:39:40 +0800

    init

---
+++ precache-manifest.76232f3d0d50c1dc7c562d1ffc658cef.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a16d0e811256e9f1bff6757a532a2fc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "856d59bde67c864708a3",
+    "url": "./static/css/2.c412fb8a.chunk.css"
+  },
+  {
+    "revision": "7c2b79b74074b7efa7d1",
+    "url": "./static/css/main.05bbd162.chunk.css"
+  },
+  {
+    "revision": "856d59bde67c864708a3",
+    "url": "./static/js/2.8f9d75be.chunk.js"
+  },
+  {
+    "revision": "7c2b79b74074b7efa7d1",
+    "url": "./static/js/main.4f0dad3b.chunk.js"
+  },
+  {
+    "revision": "156d312b20a142a8d98a",
+    "url": "./static/js/runtime-main.6d3dac84.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "./static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "./static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "./static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "./static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "./static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "./static/media/icons-20.cef8cdbb.woff"
+  }
+]);
